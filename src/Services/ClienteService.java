86c78c331ca5634fb01f188cfbfd626f44f0db32
williamdrums <williandrums@hotmail.com>
2015-04-29 19:38:45 -0400

    Foi criado pacote Service onde contem algumas classes criadas do service. by William

---
+++ src/Services/ClienteService.java
@@ -0,0 +1,46 @@
+
+package Services;
+
+/**
+ *
+ * @author Wil
+ */
+
+import java.util.List;
+
+class ClienteService {
+
+    private ClienteDAO clienteDAO;
+
+    //Contrutor
+    public ClienteService() {
+        clienteDAO = new ClienteDAO();
+    }
+
+    public void salvar(Cliente cliente) throws ServiceException {
+
+        if (cliente.getNome().isEmpty()) {
+            throw new ServiceException(" Campo nome é obrigatorio ");
+        }
+        if (cliente.getTelefone().isEmpty()) {
+            throw new ServiceException(" Campo telefone é obrigatorio ");
+        }
+         if (cliente.getEmail().isEmpty()) {
+            throw new ServiceException(" Campo Email é obrigatorio ");
+        }
+        clienteDAO.salvar(cliente);
+    }
+
+      public Cliente buscarPorId(Integer id) {
+        return clienteDAO.buscarPorId(id);
+    }
+
+    public void excluir(Integer id) {
+        clienteDAO.excluir(id);
+    }
+
+     public List<Cliente> buscarTodos() {
+        return clienteDAO.buscarTodos();
+    }
+}
+
---
+++ src/Services/ProdutoService.java
@@ -0,0 +1,43 @@
+package Services;
+
+import java.util.List;
+
+/**
+ *
+ * @author Wil
+ */
+public class ProdutoService {
+
+    class ClienteService {
+
+        private ProdutoDAO produtoDAO;
+
+        public ProdutoService() {
+            produtoDAO = new ProdutoDAO();
+        }
+
+        public void salvar(Produto produto) throws ServiceException {
+
+            if (produto.getNomeProduto().isEmpty()) {
+                throw new ServiceException(" Campo nome do produto é obrigatorio ");
+            }
+            if (produto.getDescricao().isEmpty()) {
+                throw new ServiceException(" Campo descrição é obrigatorio ");
+            }
+            produtoDAO.salvar(produto);
+        }
+
+        public Produto buscarPorId(Integer id) {
+            return produtoDAO.buscarPorId(id);
+        }
+
+        public void excluir(Integer id) {
+            produtoDAO.excluir(id);
+        }
+
+        public List<Produto> buscarTodos() {
+            return produtoDAO.buscarTodos();
+        }
+    }
+
+}
