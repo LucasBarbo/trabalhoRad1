b55b7db04deeb5f8e66fb8c66e1c174540639fad
williamdrums <williandrums@hotmail.com>
2015-05-01 22:51:03 -0400

    Foi criado pacote Service onde contem algumas classes criadas do service. by William

--- src/Services/ProdutoService.java
+++ src/Services/ProdutoService.java
@@ -12,29 +12,38 @@ import java.util.List;
 
         private ProdutoDAO produtoDAO;
 
+        //Contrutor
         public ProdutoService() {
             produtoDAO = new ProdutoDAO();
         }
 
         public void salvar(Produto produto) throws ServiceException {
 
-            if (produto.getNomeProduto().isEmpty()) {
-                throw new ServiceException(" Campo nome do produto é obrigatorio ");
+            // verifica se usuario passou vazio, apos confirmado que está vazio é lançado uma exceção com uma mensagem
+            if (produto.getId().isEmpty()) {
+                throw new ServiceException(" Campo ID do produto é obrigatorio ");
             }
             if (produto.getDescricao().isEmpty()) {
                 throw new ServiceException(" Campo descrição é obrigatorio ");
             }
+            if (produto.getValor().isEmpty()) {
+                throw new ServiceException(" Campo valor é obrigatorio");
+            }
             produtoDAO.salvar(produto);
         }
 
+        
         public Produto buscarPorId(Integer id) {
             return produtoDAO.buscarPorId(id);
         }
 
+        
+        
         public void excluir(Integer id) {
             produtoDAO.excluir(id);
         }
 
+        
         public List<Produto> buscarTodos() {
             return produtoDAO.buscarTodos();
         }
