7061ff8715d0599c803b4adecebcefe836a56c0b
Diosefer <https://bitbucket.org/Diosefer/teste>
2015-05-06 22:44:25 -0300

    Revert "Banco Cliente concluido, entretanto necessita de alterações ou melhor testes mais eficazes nos métodos buscatodos e buscaPorId"
    This reverts commit cc21b24e71b54683c09418bc971cc147a79600c4.

--- src/testeDoBanco/testeDAO.java
+++ src/testeDoBanco/testeDAO.java
@@ -6,64 +6,22 @@ import PacoteDAO.ClienteDAO;
 public class testeDAO {
 
     public static void main(String[] args) {
-
-//        testSalvar();
-//        testDeletar();
-//          testBuscatodos();
-          testAlterar();
-          
+     
+        testSalvar();
     }
-
     private static void testSalvar() {
         Cliente cli = new Cliente();
 
-        cli.setNome("Ed Motta");
-        cli.setTelefone("xburguer-xEgg");
-        cli.setEmail("Manuel@foiproceu.wawa.com.br");
+        cli.setId(001);
+        cli.setNome("Willian");
+        cli.setTelefone("xxx-xxx");
+        cli.setEmail("ffffafadfaa@faffdz");
 
+        
         ClienteDAO cliDao = new ClienteDAO();
         cliDao.salvar(cli);
-
-    }
-
-    private static void testDeletar() {
-        Cliente cli = new Cliente();
-
-        cli.setId(1);
-
-        ClienteDAO cliDAO = new ClienteDAO();
-        cliDAO.excluir(cli);
-
-    }
-
-    private static void testBuscatodos() {
-        Cliente cli = new Cliente();
-
-        ClienteDAO cliDAO = new ClienteDAO();
-        cliDAO.buscarTodos();
-
-    }
     
-    private static void testAlterar() {
-		Cliente cli = new Cliente();
-		cli.setId(3);
-		cli.setNome("William");
-		cli.setTelefone("abcdef");
-		cli.setEmail("Nor@Chuck.com.br");
 
-		ClienteDAO cliDAO = new ClienteDAO();
-		cliDAO.alterar(cli);
-	}
-    
-    private static void testBuscaPorId() {
-        Cliente cli = new Cliente();
-
-        cli.setId(3);
-
-        ClienteDAO cliDAO = new ClienteDAO();
-        cliDAO.buscarPorId(3);
-
-    }
-
-
+ }
 }
+
