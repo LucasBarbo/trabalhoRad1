a618f21e1c0de3438c4b7ae10156e3c2c3a028a8
Diosefer <https://bitbucket.org/Diosefer/teste>
2015-05-10 17:33:17 -0300

    Revert "Revert "Bancos Cliente e produto concluidos funcionando ambos""
    This reverts commit 282ade396368c1192096f45088ab4d28df50d9b8.

--- src/testeDoBanco/testeDAO.java
+++ src/testeDoBanco/testeDAO.java
@@ -1,7 +1,10 @@
 package testeDoBanco;
 
 import PacoteClasses.Cliente;
+import PacoteClasses.Produto;
 import PacoteDAO.ClienteDAO;
+import PacoteDAO.ProdutoDAO;
+import java.util.List;
 
 public class testeDAO {
 
@@ -9,17 +12,23 @@ public class testeDAO {
 
 //        testSalvar();
 //        testDeletar();
-//          testBuscatodos();
-          testAlterar();
-          
+//        testBuscatodos();
+//        testAlterar();
+//        testBuscaPorId();
+//------------------------------------
+//        testSalvarProduto();
+//        testDeletarProduto();
+//        testBuscatodosProduto();
+//        testAlterarProduto();
+        testBuscaPorIdProduto();
     }
 
     private static void testSalvar() {
         Cliente cli = new Cliente();
 
-        cli.setNome("Ed Motta");
-        cli.setTelefone("xburguer-xEgg");
-        cli.setEmail("Manuel@foiproceu.wawa.com.br");
+        cli.setNome("Steve Vai");
+        cli.setTelefone("CDEFGAB");
+        cli.setEmail("vai.steveguitar@guitars.com");
 
         ClienteDAO cliDao = new ClienteDAO();
         cliDao.salvar(cli);
@@ -37,33 +46,85 @@ public class testeDAO {
     }
 
     private static void testBuscatodos() {
-        Cliente cli = new Cliente();
 
-        ClienteDAO cliDAO = new ClienteDAO();
-        cliDAO.buscarTodos();
+        ClienteDAO cliDao = new ClienteDAO();
+        List<Cliente> listaRes = cliDao.buscarTodos();
+        System.out.print("\n");
+        for (Cliente c : listaRes) {
+            System.out.println(c.getNome() + " " + c.getTelefone() + " " + c.getEmail() + " " + c.getId());
+
+        }
 
     }
-    
+
     private static void testAlterar() {
-		Cliente cli = new Cliente();
-		cli.setId(3);
-		cli.setNome("William");
-		cli.setTelefone("abcdef");
-		cli.setEmail("Nor@Chuck.com.br");
-
-		ClienteDAO cliDAO = new ClienteDAO();
-		cliDAO.alterar(cli);
-	}
-    
-    private static void testBuscaPorId() {
         Cliente cli = new Cliente();
-
-        cli.setId(3);
+        cli.setId(5);
+        cli.setNome("Anderson Silva");
+        cli.setTelefone("abcdeffsfs");
+        cli.setEmail("spideroaranha@gmail.com");
 
         ClienteDAO cliDAO = new ClienteDAO();
-        cliDAO.buscarPorId(3);
+        cliDAO.alterar(cli);
+    }
+
+    private static void testBuscaPorId() {
+        ClienteDAO cliDAO = new ClienteDAO();
+        Cliente res = cliDAO.buscarPorId(6);
+        System.err.println(res.getNome() + " " + res.getTelefone() + " " + res.getEmail() + " " + "Id do usuário " + res.getId());
 
     }
 
+    // --------------------------------------------------------------------------------------------------
+    // TESTES DO PRODUTO
+    private static void testSalvarProduto() {
+        Produto pro = new Produto();
+
+        pro.setDescricao("Torta alemã");
+        pro.setValor("7.50");
+        
+
+        ProdutoDAO proDAO = new ProdutoDAO();
+        proDAO.salvar(pro);
+
+    }
+
+    private static void testDeletarProduto() {
+        Produto pro = new Produto();
+
+        pro.setId(1);
+
+        ProdutoDAO proDAO = new ProdutoDAO();
+        proDAO.excluir(pro);
+
+    }
+
+    private static void testBuscatodosProduto() {
+
+        ProdutoDAO proDao = new ProdutoDAO();
+        List<Produto> listaRes = proDao.buscarTodos();
+        System.out.print("\n");
+        for (Produto p : listaRes) {
+            System.out.println(p.getDescricao() + " " + p.getValor() + " " + p.getId());
+
+        }
+
+    }
+
+    private static void testAlterarProduto() {
+        Produto pro = new Produto();
+        pro.setId(6);
+        pro.setDescricao("Mouse de maracuja");
+        pro.setValor("4.50");
+        ProdutoDAO proDAO = new ProdutoDAO();
+        proDAO.alterar(pro);
+    }
+
+    private static void testBuscaPorIdProduto() {
+        ProdutoDAO proDAO = new ProdutoDAO();
+        Produto res = proDAO.buscarPorId(3);
+        System.err.println(res.getDescricao() + " " + res.getValor() + " "+ "O id do produto é"+" "+""+ res.getId());
+
+    }
 
 }
